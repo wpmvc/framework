ff7e75ae94eb7882ae156213fd82fa2ea53137a8
mdalaminbey <mdalaminbey@gmail.com>
2024-09-27 19:46:11 +0600

    added filter hook support in rest APIs middleware

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -15,12 +15,13 @@ class RouteServiceProvider extends WpMVCRouteServiceProvider implements Provider
         $config = App::$config->get( 'app' );
 
         parent::$properties = [
-            'rest'                      => $config['rest_api'],
-            'ajax'                      => $config['ajax_api'],
-            'middleware'                => $config['middleware'],
-            'routes-dir'                => App::get_dir( "routes" ),
-            'rest_response_action_hook' => $config['rest_response_action_hook'] ?? '',
-            'rest_response_filter_hook' => $config['rest_response_filter_hook'] ?? ''
+            'rest'                        => $config['rest_api'],
+            'ajax'                        => $config['ajax_api'],
+            'middleware'                  => $config['middleware'],
+            'routes-dir'                  => App::get_dir( "routes" ),
+            'rest_response_action_hook'   => $config['rest_response_action_hook'] ?? '',
+            'rest_response_filter_hook'   => $config['rest_response_filter_hook'] ?? '',
+            'rest_permission_filter_hook' => $config['rest_permission_filter_hook'] ?? ''
         ];
 
         parent::boot();
